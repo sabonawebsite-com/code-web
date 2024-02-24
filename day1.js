b87eebf530dbab3a195388b95a723c02d1661755
sabonawebsite-com <154590543+sabonawebsite-com@users.noreply.github.com>
2024-02-24 16:06:12 +0300

    Delete day1.js

--- day1.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("myh1").textContent = "hello guys ";
-document.getElementById("myp1").textContent = "you par";
-let age = 45;
-let grade = 3.5;
-let car = 3;
-console.log(`you are ${age} years old `);
-console.log(`grade is ${grade} that is good`);
-console.log(`ther are three ${car} in the park`)
