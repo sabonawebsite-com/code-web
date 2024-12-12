d47d6b2358f226715de40d374b3bd77c7e15232e
sabonawebsite-com <154590543+sabonawebsite-com@users.noreply.github.com>
2024-12-12 23:38:39 +0300

    Add files via upload

---
+++ udacity02.js
@@ -0,0 +1,13 @@
+function doSub(){
+  
+
+
+    let newName=window.prompt("what is your name?..")
+    if (newName=="fanta") {
+        alert("you are boy")
+        
+    } else {
+        alert("you are not CS go out")
+    }
+
+}
